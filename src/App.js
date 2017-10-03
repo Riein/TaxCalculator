1666e2b9168adb5d6b107b6fa31fc0662bef8bdc
Sasha Demyanik <ttsvan@msn.com>
2017-10-02 17:38:00 -0700

    Adds a state prompt, begins initial setup.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    id:'',
+    state:'',
+    submitted: false
+  }
+
+}
+
   render() {
+    var state;
+    if (this.state.state && this.state.submitted === false) {
+
+    } else if (!this.state.state && this.state.submitted === false) {
+      state = <span>
+        <h2>Please enter your state (Washington or Oregon)</h2>
+        <form>
+          <input type="text" placeHolder="Enter State..." ref="state" />
+        </form>
+      </span>
+    } else if (this.state.submitted === true) {
+
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div className="App text-center">
+        <div className="App-header">
+          <h2>Income Tax Calculator</h2>
+        </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This application will calculate how much income and sales tax you pay in Washington or Oregon.
         </p>
+        <div className="text-center">
+          {state}
+        </div>
       </div>
     );
   }
