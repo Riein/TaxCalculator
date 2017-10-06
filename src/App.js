e4760d4c5a413a9eebeedd04155e4420707762f8
Sasha Demyanik <ttsvan@msn.com>
2017-10-05 17:57:50 -0700

    Splits Autocomplete into its own component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import Autocomplete from 'react-autocomplete';
+import Search from './components/Search';
 import './App.css';
-import {fakeRequest} from './utils';
 
 
 class App extends Component {
@@ -14,22 +13,8 @@ constructor(props) {
     loading:false,
     submitted: false
   }
-  this.requestTimer = null
-  this.renderItems = this.renderItems.bind(this)
 
 }
-
-renderItems(items) {
-    return items.map((item, index) => {
-      const text = item.props.children
-      if (index === 0 || items[index - 1].props.children.charAt(0) !== text.charAt(0)) {
-        return [<div className="item item-header">{text.charAt(0)}</div>, item]
-      }
-      else {
-        return item
-      }
-    })
-  }
 
 
   render() {
@@ -56,38 +41,7 @@ renderItems(items) {
             <label htmlFor="states-autocomplete">Choose a state from the US</label>
           </div>
           <div className="item-center">
-            <Autocomplete
-              value={this.state.value}
-              inputProps={{ id: 'states-autocomplete' }}
-              wrapperStyle={{ position: 'relative', display: 'inline-block' }}
-              items={this.state.statesList}
-              getItemValue={(item) => item.name}
-              onSelect={(value, state) => this.setState({ value, statesList: [state] }) }
-              onChange={(event, value) => {
-                this.setState({ value, loading: true, statesList: [] })
-                clearTimeout(this.requestTimer)
-                this.requestTimer = fakeRequest(value, (items) => {
-                  this.setState({ statesList: items, loading: false })
-                })
-              }}
-              renderItem={(item, isHighlighted) => (
-                <div
-                  className={`item ${isHighlighted ? 'item-highlighted' : ''}`}
-                  key={item.abbr}
-                  >{item.name}</div>
-                )}
-                renderMenu={(items, value) => (
-                  <div className="menu">
-                  {value === '' ? (
-                    <div className="item">Type of the name of a United State</div>
-                  ) : this.state.loading ? (
-                    <div className="item">Loading...</div>
-                  ) : items.length === 0 ? (
-                    <div className="item">No matches for {value}</div>
-                  ) : this.renderItems(items)}
-                  </div>
-                )}
-            />
+            <Search />
           </div>
         </div>
       </div>
---
+++ src/components/Search.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import Autocomplete from 'react-autocomplete';
+import '../App.css';
+import {fakeRequest} from '../utils';
+
+
+class Search extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    id:'',
+    value:'',
+    statesList: [],
+    loading:false,
+    submitted: false
+  }
+  this.requestTimer = null
+  this.renderItems = this.renderItems.bind(this)
+
+}
+
+renderItems(items) {
+    return items.map((item, index) => {
+      const text = item.props.children
+      if (index === 0 || items[index - 1].props.children.charAt(0) !== text.charAt(0)) {
+        return [<div className="item item-header">{text.charAt(0)}</div>, item]
+      }
+      else {
+        return item
+      }
+    })
+  }
+
+
+  render() {
+    var value;
+    if (this.state.value && this.state.submitted === false) {
+
+    } else if (!this.state.value && this.state.submitted === false) {
+      value = <span>
+
+      </span>
+    } else if (this.state.submitted === true) {
+
+    }
+    return (
+      <div className="App text-center">
+        <Autocomplete
+          value={this.state.value}
+          inputProps={{ id: 'states-autocomplete' }}
+          wrapperStyle={{ position: 'relative', display: 'inline-block' }}
+          items={this.state.statesList}
+          getItemValue={(item) => item.name}
+          onSelect={(value, state) => this.setState({ value, statesList: [state] }) }
+          onChange={(event, value) => {
+            this.setState({ value, loading: true, statesList: [] })
+            clearTimeout(this.requestTimer)
+            this.requestTimer = fakeRequest(value, (items) => {
+              this.setState({ statesList: items, loading: false })
+            })
+          }}
+          renderItem={(item, isHighlighted) => (
+            <div
+              className={`item ${isHighlighted ? 'item-highlighted' : ''}`}
+              key={item.abbr}
+              >{item.name}</div>
+            )}
+            renderMenu={(items, value) => (
+              <div className="menu">
+              {value === '' ? (
+                <div className="item">Type a state...</div>
+              ) : this.state.loading ? (
+                <div className="item">Loading...</div>
+              ) : items.length === 0 ? (
+                <div className="item">No matches for {value}</div>
+              ) : this.renderItems(items)}
+              </div>
+            )}
+        />
+      </div>
+    );
+  }
+}
+
+export default Search;
