e8d034229c2d05ca392a38f94759b0c370ec730a
Sasha Demyanik <ttsvan@msn.com>
2017-11-20 18:07:34 -0800

    Additional changes to Income component class.

--- src/components/Income.js
+++ src/components/Income.js
@@ -4,22 +4,28 @@ class Income extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value:props.value
+      value:0
     };
   }
 
   handleChange = (e) => {
     //console.log(e.target.value);
     const re = /^[0-9\b]+$/;
-    if (e.target.value == '' || re.test(e.target.value)) {
-       this.setState({value: e.target.value})
+    console.log("I have entered with a value of " + e.target.value);
+    if (e.target.value === '' || re.test(e.target.value)) {
+       this.setState({value: e.target.value}, () => {
+         this.props.onChange(this.state.value);
+       });
        //console.log('This is an integer');
-       console.log(this.state.value);
+
+       console.log("Changed the state value to " + this.state.value);
     } else {
       console.log('Income value is NOT an integer');
     }
+    this.props.onChange(this.state.value);
   }
 
+ /*  */
   handleKeyPress = (e) => {
     if (e.key === 'Enter') {
       this.setState({value: e.target.value});
